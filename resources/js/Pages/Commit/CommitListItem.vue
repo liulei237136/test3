9abd6178b3e9eb657386f311df7a481c2daf1d2f
liu lei <liulei237136@gmail.com>
2022-01-21 12:21:00 +0800

    commitlistitem

--- resources/js/Pages/Commit/CommitListItem.vue
+++ resources/js/Pages/Commit/CommitListItem.vue
@@ -1,17 +1,32 @@
 <template>
   <div :class="classes">
     <h3>{{ commit.title }}</h3>
-    <p class="text-sm">
-      <Link
-        href="#"
-        class="hover:underline"
-        :title="`查看${commit.author.name}的所有保存`"
-        >{{ commit.author.name }}</Link
-      >
-      <span class="ml-4 text-gray-500"
-        >创建于 {{ new Date(commit.created_at).toLocaleString() }}</span
-      >
-    </p>
+    <div class="flex items-center">
+      <Link href="#">
+        <button
+          v-if="$page.props.jetstream.managesProfilePhotos"
+          class="flex mt-1 mr-2 text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition"
+        >
+          <img
+            class="h-8 w-8 rounded-full object-cover"
+            :src="$page.props.user.profile_photo_url"
+            :alt="$page.props.user.name"
+          />
+        </button>
+      </Link>
+
+      <p class="text-sm">
+        <Link
+          href="#"
+          class="hover:underline"
+          :title="`查看${commit.author.name}的所有保存`"
+          >{{ commit.author.name }}</Link
+        >
+        <span class="ml-4 text-gray-500"
+          >创建于 {{ new Date(commit.created_at).toLocaleString() }}</span
+        >
+      </p>
+    </div>
   </div>
 </template>
 
